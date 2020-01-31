ecfc094ea4eeedfc3fa0bbcbad742ebe5c1cdfa6
Kyle Honeycutt <coinables@gmail.com>
2020-01-30 21:56:17 -0800

    update api
    smartbit dropped support for xpubs. switched to new api provider blockchair

--- xpub.php
+++ xpub.php
@@ -2,10 +2,15 @@
 
 //replace with your BIP32 extended public key
 $xpub = "xpub6CUGRUonZSQ4TWtTMmzXdrXDtypWKiKrhko4egpiMZbpiaQL2jkwSB1icqYh2cfDfVxdx4df189oLKnC5fSwqPfgyP3hooxujYzAu3fDVmz";
-$url = "https://api.smartbit.com.au/v1/blockchain/address/".$xpub;
-
+$url = "https://api.blockchair.com/bitcoin/dashboards/xpub/".$xpub;
 $fgc = json_decode(file_get_contents($url), true);
-$next = $fgc["address"]["extkey_next_receiving_address"];
+
+$addrlen = count($fgc["context"]["checked"]);
+$lessfifty = $addrlen - 50;
+
+$context = $fgc["context"]["checked"][$lessfifty];
+$expl = explode(": ",$context);
+$next = $expl[1];
 
 ?>
 
